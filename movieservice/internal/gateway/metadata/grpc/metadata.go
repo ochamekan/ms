b57e31d20df6a772f2381876356e9d8f44d428b8
ochamekan <yanikidev@gmail.com>
2025-12-10 19:09:48 +0200

    movie service: add retries

--- movieservice/internal/gateway/metadata/grpc/metadata.go
+++ movieservice/internal/gateway/metadata/grpc/metadata.go
@@ -2,11 +2,14 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ochamekan/ms/gen"
 	"github.com/ochamekan/ms/internal/grpcutil"
 	"github.com/ochamekan/ms/metadataservice/pkg/model"
 	"github.com/ochamekan/ms/pkg/discovery"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Gateway struct {
@@ -25,12 +28,22 @@ func (g *Gateway) GetMetadata(ctx context.Context, id int) (*model.Metadata, err
 	defer conn.Close()
 
 	client := gen.NewMetadataServiceClient(conn)
-	resp, err := client.GetMetadata(ctx, &gen.GetMetadataRequest{Id: int32(id)})
-	if err != nil {
-		return nil, err
+	var resp *gen.GetMetadataResponse
+
+	// If error is retriable, try 5 times and if no success return err
+	const maxRetries = 5
+	for range 5 {
+		resp, err = client.GetMetadata(ctx, &gen.GetMetadataRequest{Id: int32(id)})
+		if err != nil {
+			if shouldRetry(err) {
+				continue
+			}
+			return nil, err
+		}
+		return model.MetadataFromProto(resp.Metadata), nil
 	}
 
-	return model.MetadataFromProto(resp.Metadata), nil
+	return nil, fmt.Errorf("GetMetadata failed after %d retries: %s", maxRetries, err.Error())
 }
 
 func (g *Gateway) PutMetadata(ctx context.Context, title, description, director string, year int) error {
@@ -47,4 +60,12 @@ func (g *Gateway) PutMetadata(ctx context.Context, title, description, director 
 	}
 
 	return nil
+}
+
+func shouldRetry(err error) bool {
+	e, ok := status.FromError(err)
+	if !ok {
+		return false
+	}
+	return e.Code() == codes.DeadlineExceeded || e.Code() == codes.ResourceExhausted || e.Code() == codes.Unavailable
 }
